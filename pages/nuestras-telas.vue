d12045ff7521944de20fd497e3b47cc6e884325b
Esther Rodriguez <estherrodriguez@MacBook-Pro-de-Esther.local>
2020-09-13 13:13:35 +0200

    add updates to fabrics

--- pages/nuestras-telas.vue
+++ pages/nuestras-telas.vue
@@ -1,10 +1,10 @@
 <template>
   <b-container class="telas">
-    <div class="d-flex justify-content-center flex-column">
+    <!-- <div class="d-flex justify-content-center flex-column">
       <h1 class="mx-auto py-4">Nuestras telas</h1>
       <b-img src="/images/telas-upenda.jpg" class="w-75 mx-auto" fluid alt="Nuestras Telas"></b-img>
-    </div>
-    <!-- <div v-for="fabric in fabrics" :key="fabric.id" class="d-flex flex-column">
+    </div> -->
+    <div v-for="fabric in fabrics" :key="fabric.id" class="d-flex flex-column align-items-center">
         <b-img class="fabric-image d-flex w-25" :src="fabric.image" :alt="fabric.name">
           </b-img>
         <b-row class="d-flex fabrics-container">
@@ -12,7 +12,7 @@
             <b-img class="fabric-image d-flex" :src="imageFabric.image" :alt="imageFabric.name">{{imageFabric.image}}</b-img>
           </div>
       </b-row>
-    </div> -->
+    </div>
   </b-container>
 </template>
 
@@ -38,9 +38,8 @@ export default {
 
 <style lang="scss" scoped>
 .fabrics-container {
-  max-width: 800px;
   display: flex;
-  margin: auto;
+  justify-content:center;
   .fabric-image {
     max-width:250px;
     display:flex;
